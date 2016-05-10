2ef02c6473b70ced360e0aeda864db8e36d0e763
Val95240 <Valentin.GUILLET@student.isae-supaero.fr>
2016-05-10 16:38:43 +0200

    Update Objets.py

--- Objets.py
+++ Objets.py
@@ -1,16 +1,16 @@
-#from pylab import *
-from random import random
+# -*- coding: cp1252 -*-
+from random import random, randrange, choice
 from math import e, pi
-from tkinter import *
+#from tkinter import *
 
 
 ###############################################################################################################
 # Tkinter
 haut, larg = 900, 900
 
-fenetre = Tk()
-canvas = Canvas(fenetre, width = larg, height = haut, bg = "white")
-canvas.grid()
+#fenetre = Tk()
+#canvas = Canvas(fenetre, width = larg, height = haut, bg = "white")
+#canvas.grid()
 
 
 ###############################################################################################################
@@ -23,6 +23,7 @@ class Vertex():
         self.coordX = 0
         self.coordY = 0
         self.adjacence = []
+        self.edges = []
         self.chemin = []
         
     def __str__(self):
@@ -31,6 +32,9 @@ class Vertex():
     def __repr__(self):
         return str(self.id)
         #return "Je suis le sommet {0}, situé aux coordonnées ({1}, {2})".format(str(self.id), str(self.coordX), str(self.coordY))
+
+    def __int__(self):
+        return int(self.id)
         
     def getCoord(self, tailleGraph):
         """Mets à jour les coordonnés du vertex en vu de l'affichage"""
@@ -71,11 +75,13 @@ class Graph():
             for idNumb in v.adjacence:
                 canvas.create_line(v.coordX, v.coordY, self.listeSommets[idNumb].coordX, self.listeSommets[idNumb].coordY, fill = "red")
 
-    def addEdge(self, i, j):
+    def addEdge(self, i, j, sure = False):
         """Ajoute un edge entre les sommets i et j avec la probabilité probaEdge"""
-        if random() < self.probaEdge:
+        if random() < self.probaEdge or sure:
             self.listeSommets[i].adjacence.append(j)
+            self.listeSommets[i].edges.append(True)
             self.listeSommets[j].adjacence.append(i)
+            self.listeSommets[j].edges.append(True)
 
     def isConnex(self):
         """Retourne si le graphe est connexe ou non"""
@@ -98,13 +104,13 @@ class Graph():
         return self.isConnex() and all([vertex.getDegre()%2 == 0 for vertex in self.listeSommets])
 
 
-    def findCycle(self):
-        """Retourne un cycle dans le graphe"""
+    def findCycle(self, debut = 0):
+        """Retourne un cycle dans le graphe"""        
         for v in self.listeSommets:
             v.chemin = []
             
         checked = set()
-        liste = [self.listeSommets[0]]
+        liste = [self.listeSommets[debut]]
         for n in range(self.taille+5):
             vertex = liste.pop(0)
             checked.add(vertex.id)
@@ -116,6 +122,50 @@ class Graph():
                     else:
                         v.chemin = vertex.chemin + [vertex.id]
                     liste.append(v)
+
+    def findEulerianTour(self):
+
+        if not self.existeTour():
+            return "Il n'y a pas de tour d'Euler possible dans ce graphe"
+
+        else:
+            return self.findEulerianTourAux(0)
+
+    def findEulerianTourAux(self, vertex):
+        """Retourne un tour d'Euler dans le graphe"""
+
+        # Cas initial :
+        # si tous les edges partant de vertex ont déjà été utilisés dans le tour, on retourne rien
+        if True not in self.listeSommets[vertex].edges:
+            print("Une feuille !")
+            print(vertex)
+            print(self.listeSommets[vertex].edges)
+            return [vertex]
+
+        # Récurrence :
+        # On trouve un cycle
+        cycle = self.findCycle(vertex)
+        print("Vertex :", vertex)
+        print("Cycle : ", cycle)
+        print()
+
+        # On met à jour les vertex utilisés dans le cycle :
+        for i in range(len(cycle)-1):
+            v = self.listeSommets[cycle[i]]
+            print("Voisin : ", v)
+            index =  v.adjacence.index(cycle[i+1])                  # Index d'apparition du vertex \cycle[i+1]\ dans la table d'adjacence
+            v.edges[index] = False
+            print("Voisin.edge apres maj : ", v.edges)
+            
+
+        # Pour chaque sommets de ce cycle, on va trouver un nouveau cycle
+        tour = []
+        for i in range(len(cycle)-1):
+            tour.append(self.findEulerianTourAux(cycle[i]))
+
+        return tour
+        
+            
 
 
 
@@ -133,9 +183,37 @@ def createGraph(n = 50, p = 0.25):
     return graph
 
 
+def makeEulerian(graph):
+    """Ajoute des edges à un graphe jusqu'à ce que celui ci contienne un tour d'Euler"""
+
+    wrongVertices = [vertex for vertex in graph.listeSommets if vertex.getDegre()%2 == 1 or vertex.getDegre() == 0]
+    while wrongVertices != []:
+        vertex = wrongVertices.pop(0)
+        choix = list(filter(lambda v:v.id not in vertex.adjacence, wrongVertices))
+        if choix != []:
+            voisin = choice(choix)
+        else:
+            voisin = choice(list(filter(lambda v:v.id not in vertex.adjacence and v.id != vertex.id, graph.listeSommets)))
+            wrongVertices.append(voisin)
+        graph.addEdge(vertex.id, voisin.id, True)
+        if voisin.getDegre() != 1 and choix != []:
+            wrongVertices.remove(voisin)
+        if vertex.getDegre() == 1:
+            wrongVertices.append(vertex)
 
 
 def affiche(graph):
     """Affiche un graphe"""
     canvas.delete("all")
     graph.drawGraph()
+
+
+g = createGraph(50, 0.5)
+print(sum([v.getDegre() for v in g.listeSommets]))
+print("The graph is connex : ", g.isConnex())
+print("The graph has an Eulerian tour : ", g.existeTour())
+makeEulerian(g)
+print("After modification , the graph has an Eulerian tour : ", g.existeTour())
+for v in g.listeSommets:
+    print str(v) + " : " + ", ".join(map(str, v.adjacence))
+print(g.findEulerianTour())
